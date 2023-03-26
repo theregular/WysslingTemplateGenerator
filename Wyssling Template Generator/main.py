ee1544f1a96d00a3796197376ae15f772d314580
Ben Kartchner <brkartch@gmail.com>
2023-03-26 13:48:47 -0600

    added Reset Button

--- Wyssling Template Generator/main.py
+++ Wyssling Template Generator/main.py
@@ -100,7 +100,8 @@ def generateWindow(inWindow):
 
 entries = []
 #userInput = []
-rowNum = 0
+#global rowNum
+#rowNum = 0
 #colNum = 0
 def generateRow(inWindow, autofill=""):
     label = tk.Label(inWindow, text=categories[rowNum])
@@ -113,14 +114,13 @@ def generateRow(inWindow, autofill=""):
     
 def get_input():
     userInput = []
-    for entry in entries:
-        input = entry.get()
+    for currEntry in entries:
+        input = currEntry.get()
         userInput.append(input)
-    print(accessChoice.get())
-    print(stateChoice.get()) #TODO: change template based on state input
-    print(asceChoice.get())
-    print(exposChoice.get())
-
+    #print(accessChoice.get())
+    #print(stateChoice.get()) #TODO: change template based on state input
+    #print(asceChoice.get())
+    #print(exposChoice.get())
 
     userInput.append(accessChoice.get())
     userInput.append(stateChoice.get())
@@ -128,130 +128,156 @@ def get_input():
     userInput.append(exposChoice.get())
 
     print(userInput)
-    print("INPUT LENGTH: " + str(len(userInput)))
+    #print("INPUT LENGTH: " + str(len(userInput)))
     docedit.setUserInput(userInput)
     docedit.run()
+
+def reset():
+    #print("RESET CALLED")
+    for widget in window.winfo_children():
+        widget.destroy()
+    generateWidgets()
+
+#window = tk.Tk()
+#window.title("Wyssling Template Generator")
+#window.minsize(500,400)
+#window.resizable(False,False)
+
+#generateWindow(window)
+
+def generateWidgets():
+    global rowNum
+    rowNum = 0
+    #name
+    generateRow(window)
+    #generateRow(window, docedit.name_fill)
+    rowNum+=1
+
+    #address
+    generateRow(window)
+    #generateRow(window, docedit.address_fill)
+    rowNum+=1
+
+    #state
+    #generateRow(window, docedit.state_fill)
+    label = tk.Label(window, text=categories[rowNum])
+    label.grid(row=rowNum, column=0)
+ 
+    global stateChoice #TODO: change from global variable
+    stateChoice = tk.StringVar(window)
+    stateChoice.set(states[30]) #sets NJ as default
+    stateList = tk.OptionMenu(window, stateChoice, *states)
+    stateList.grid(row=rowNum, column = 1)
+
+    rowNum+=1
+
+    #systemSize
+    generateRow(window, docedit.systemSize_fill)
+    rowNum+=1
+
+    #framing
+    generateRow(window, docedit.framing_fill)
+    rowNum+=1
+
+    #roofMaterial
+    generateRow(window, docedit.roofMaterial_fill)
+    rowNum+=1
+
+    #slope
+    generateRow(window, docedit.slope_fill)
+    rowNum+=1
+
+    #atticAccess
+    #generateRow(window, docedit.atticAccess_fill)
+    label = tk.Label(window, text=categories[rowNum])
+    label.grid(row=rowNum, column=0)
+
+    global accessChoice
+    accessChoice = tk.StringVar(value="Accessible")
+    radio1 = tk.Radiobutton(window, text="Accessible", value="Accessible", variable=accessChoice)
+    radio2 = tk.Radiobutton(window, text="Inaccessible", value="Inaccessible", variable=accessChoice)
+    radio1.grid(row=rowNum, column=1)
+    radio2.grid(row=rowNum, column=2)
+
+    rowNum+=1
+
+    #existingDead
+    generateRow(window, docedit.existingDead_fill)
+    rowNum+=1
+
+    #newDead
+    generateRow(window, docedit.newDead_fill)
+    rowNum+=1
+
+    #totalDead #doesn't need to be generated
+    #generateRow(window, docedit.totalDead_fill)
+    #rowNum+=1
+
+    #snow
+    generateRow(window, docedit.snow_fill)
+    rowNum+=1
+
+    #asce
+    #generateRow(window, docedit.asce_fill)
+    label = tk.Label(window, text=categories[rowNum])
+    label.grid(row=rowNum, column=0)
+
+    global asceChoice
+    asceChoice = tk.StringVar(window)
+    asceChoice.set(asces[3]) #sets 16 as default
+    asceList = tk.OptionMenu(window, asceChoice, *asces)
+    asceList.grid(row=rowNum, column = 1)
+
+    rowNum+=1
+
+    #wind
+    generateRow(window, docedit.wind_fill)
+    rowNum+=1
+
+    #exposure category
+    #generateRow(window, docedit.codeYear_fill)
+    label = tk.Label(window, text=categories[rowNum])
+    label.grid(row=rowNum, column=0)
+
+    global exposChoice
+    exposChoice = tk.StringVar(value="B")
+    radio1 = tk.Radiobutton(window, text="B", value="B", variable=exposChoice)
+    radio2 = tk.Radiobutton(window, text="C", value="C", variable=exposChoice)
+    radio1.grid(row=rowNum, column=1)
+    radio2.grid(row=rowNum, column=2)
+
+    rowNum+=1
+
+    #code year #TODO: handle florida code/other weird codes
+    generateRow(window, docedit.codeYear_fill)
+    rowNum+=1
+
+    #mount type
+    generateRow(window, docedit.mountingType_fill)
+    rowNum+=1
+
+    #mount info
+    label = tk.Label(window, text=categories[rowNum])
+    label.grid(row=rowNum, column=0)
+    rowNum+=1
+
+    #spacing
+    generateRow(window, docedit.spacing_fill)
+    rowNum+=1
+
+    #generate button
+    genButton = tk.Button(window, text="Generate", command=get_input)
+    genButton.grid(row=rowNum, column=0)
+
+    #reset button #TODO:implement
+    resetButton = tk.Button(window, text="Reset", command=reset)
+    resetButton.grid(row=rowNum, column=1)
 
 window = tk.Tk()
 window.title("Wyssling Template Generator")
 window.minsize(500,400)
 window.resizable(False,False)
 
-#generateWindow(window)
-
-#name
-generateRow(window, docedit.name_fill)
-rowNum+=1
-
-#address
-generateRow(window, docedit.address_fill)
-rowNum+=1
-
-#state
-#generateRow(window, docedit.state_fill)
-label = tk.Label(window, text=categories[rowNum])
-label.grid(row=rowNum, column=0)
-
-stateChoice = tk.StringVar(window)
-stateChoice.set(states[30]) #sets NJ as default
-stateList = tk.OptionMenu(window, stateChoice, *states)
-stateList.grid(row=rowNum, column = 1)
-
-rowNum+=1
-
-#systemSize
-generateRow(window, docedit.systemSize_fill)
-rowNum+=1
-
-#framing
-generateRow(window, docedit.framing_fill)
-rowNum+=1
-
-#roofMaterial
-generateRow(window, docedit.roofMaterial_fill)
-rowNum+=1
-
-#slope
-generateRow(window, docedit.slope_fill)
-rowNum+=1
-
-#atticAccess
-#generateRow(window, docedit.atticAccess_fill)
-label = tk.Label(window, text=categories[rowNum])
-label.grid(row=rowNum, column=0)
-
-accessChoice = tk.StringVar(value="Accessible")
-radio1 = tk.Radiobutton(window, text="Accessible", value="Accessible", variable=accessChoice)
-radio2 = tk.Radiobutton(window, text="Inaccessible", value="Inaccessible", variable=accessChoice)
-radio1.grid(row=rowNum, column=1)
-radio2.grid(row=rowNum, column=2)
-
-rowNum+=1
-
-#existingDead
-generateRow(window, docedit.existingDead_fill)
-rowNum+=1
-
-#newDead
-generateRow(window, docedit.newDead_fill)
-rowNum+=1
-
-#totalDead #doesn't need to be generated
-#generateRow(window, docedit.totalDead_fill)
-#rowNum+=1
-
-#snow
-generateRow(window, docedit.snow_fill)
-rowNum+=1
-
-#asce
-#generateRow(window, docedit.asce_fill)
-label = tk.Label(window, text=categories[rowNum])
-label.grid(row=rowNum, column=0)
-
-asceChoice = tk.StringVar(window)
-asceChoice.set(asces[3]) #sets 16 as default
-asceList = tk.OptionMenu(window, asceChoice, *asces)
-asceList.grid(row=rowNum, column = 1)
-
-rowNum+=1
-
-#wind
-generateRow(window, docedit.wind_fill)
-rowNum+=1
-
-#exposure category
-#generateRow(window, docedit.codeYear_fill)
-label = tk.Label(window, text=categories[rowNum])
-label.grid(row=rowNum, column=0)
-
-exposChoice = tk.StringVar(value="B")
-radio1 = tk.Radiobutton(window, text="B", value="B", variable=exposChoice)
-radio2 = tk.Radiobutton(window, text="C", value="C", variable=exposChoice)
-radio1.grid(row=rowNum, column=1)
-radio2.grid(row=rowNum, column=2)
-
-rowNum+=1
-
-#code year #TODO: handle florida code/other weird codes
-generateRow(window, docedit.codeYear_fill)
-rowNum+=1
-
-#mount type
-generateRow(window, docedit.mountingType_fill)
-rowNum+=1
-
-#mount info
-label = tk.Label(window, text=categories[rowNum])
-label.grid(row=rowNum, column=0)
-rowNum+=1
-
-#spacing
-generateRow(window, docedit.spacing_fill)
-rowNum+=1
-
-#print(rowNum)
-button = tk.Button(window, text="Generate", command=get_input)
-button.grid()
+generateWidgets()
 
 window.mainloop()
